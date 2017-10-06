2a2d38dfe5fa8e57a098d673ed8aea057dce4cb8
Sebastian Wojnicz <sebulw@gmail.com>
2017-10-06 11:02:05 +0200

    ready to check

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,14 +1,14 @@
-$(function() {
-	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 5000);
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-640}, 1000, moveFirstSlide);
-	}
-	changeSlide();
-	function moveFirstSlide() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	}
-});
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+Phone.prototype.printInfo = function() {
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+}
+var samsungGalaxyS6 = new Phone('Samsung', 2100, 'grey'),
+	iPhone6S = new Phone('Apple', 2850, 'black'),
+	onePlusOne = new Phone('One Plus', 2200, 'silver');
+samsungGalaxyS6.printInfo();
+iPhone6S.printInfo();
+onePlusOne.printInfo();
